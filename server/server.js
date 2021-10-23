0dac9b0d44a6cf3c3f8f89494d1069550a6ab6d0
Hasan Al <Hasanalghanim@gmail.com>
2021-10-23 00:27:09 -0600

    Initial Setup
    Initial Set Up
    
    BACKEND
    -Express setup
    -routes / config setup
    -dontENV setup
    
    FRONTEND
    -reacts setup

---
+++ server/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const dataRouter = require('./routes/data');
+const app = express();
+
+// database routing
+// const db = require('./config/database');
+// Test DB
+// db.authenticate()
+// 	.then(() => console.log('Connection has been established successfully.'))
+// 	.catch((err) => console.error('Unable to connect to the database:', err));
+
+// // Syncs models to database
+// db.sync({ alter: true });
+
+// Body parser middlewares
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Ducks API index Page');
+});
+
+app.use('/data', dataRouter);
+
+// Run sevrer
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
